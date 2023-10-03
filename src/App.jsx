93b2d42899c258c307ef933f2cf80ea365630ca9
Hunter Andersen <andersen.hunter42@gmail.com>
2023-10-03 07:52:28 -0700

    Partway through example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [people, setPeople] = useState([{"name":"Yoda"}, {"name":"Leia"}]);
+
+  //How do I render a list of things in React?
   return (
     <div>
-      
+      <h1>Star Wars People</h1>
+      <ul>
+        {people.map((person) => {
+          return <li>{person.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
